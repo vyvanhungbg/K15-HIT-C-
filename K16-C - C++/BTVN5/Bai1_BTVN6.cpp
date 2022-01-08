4c1a4e68034618fcaa166f97a07e673253873368
Hung <vyvanhung2882001bg.com>
2022-01-08 09:18:26 +0700

    BTVN5-format-code

--- K16-C - C++/BTVN5/Bai1_BTVN6.cpp
+++ K16-C - C++/BTVN5/Bai1_BTVN6.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 void NhapMang(int a[], int n)
 {
-	for(int i=0; i<n; i++)
+    for(int i=0; i<n; i++)
     {
-		do
+        do
         {
             cin >> a[i];
             if(a[i] <= 0 )
                 cout<<"Nhap so nguyen duong . Nhap lai"<<endl;
-        }while(a[i] <= 0);
-	}
+        }
+        while(a[i] <= 0);
+    }
 }
 
 //void NhapMang(int a[], int &n)  /// neu nhap ca n trong ham thi can truyen & de n nhan gia tri khi ra khoi ham
@@ -34,66 +35,77 @@ void NhapMang(int a[], int n)
 
 void XuatMang(int a[], int n)
 {
-	for(int i=0; i<n; i++)
-		cout << a[i] << " ";
+    for(int i=0; i<n; i++)
+        cout << a[i] << " ";
 }
 
-bool SoDenDui(int n) {
-	while(n != 0)
+bool SoDenDui(int n)
+{
+    while(n != 0)
     {
-		int du = n % 10;
-		if(du != 4 && du != 7) {  /// xuat hien so khac 4 va 7 thi sai luon
-			return false;
-		}
-		n = n / 10;
-	}
-	return true;
-}
-
-void XoaChiSo(int a[], int &n, int viTri) {   /// xoa thi so luong phan tu giam can truyen &n
-	for(int i=viTri; i<n-1; i++) {
-		a[i] = a[i+1];
-	}
-	n--;
-}
-
-void XoaTatCaCacSoDenDui(int a[], int &n) {
-	for(int i=0; i<n; i++) {
-		if(SoDenDui(a[i])) {
-			XoaChiSo(a, n, i);
-			i--;   /// i-- de tranh loi 2 phan so den dui dung gan nhau , co the dung while thi khong phai i--
-		}
-	}
-}
-
-void SapXep(int a[], int n) {
-	for(int i=0; i<n; i++)
-    {
-		for(int j=i+1; j<n; j++)
+        int du = n % 10;
+        if(du != 4 && du != 7)    /// xuat hien so khac 4 va 7 thi sai luon
         {
-			if(a[i] > a[j])
-			{
-				int temp = a[i];
-				a[i] = a[j];
-				a[j] = temp;
-			}
-		}
-	}
+            return false;
+        }
+        n = n / 10;
+    }
+    return true;
 }
 
-int main() {
+void XoaChiSo(int a[], int &n, int viTri)     /// xoa thi so luong phan tu giam can truyen &n
+{
+    for(int i=viTri; i<n-1; i++)
+    {
+        a[i] = a[i+1];
+    }
+    n--;
+}
 
-	int n;
+void XoaTatCaCacSoDenDui(int a[], int &n)
+{
+    for(int i=0; i<n; i++)
+    {
+        if(SoDenDui(a[i]))
+        {
+            XoaChiSo(a, n, i);
+            i--;   /// i-- de tranh loi 2 phan so den dui dung gan nhau , co the dung while thi khong phai i--
+        }
+    }
+}
+
+void SapXep(int a[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(a[i] > a[j])
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int n;
     do
     {
-        cout<<"n = ";cin>>n;
+        cout<<"n = ";
+        cin>>n;
         if(n <= 0 || n >= 100)
             cout<<"Sai dieu kien . Nhap lai"<<endl;
-    }while(n <= 0 || n >= 100);
-	int a[n];
-	NhapMang(a, n);
-	XoaTatCaCacSoDenDui(a, n);
-	SapXep(a, n);
-	XuatMang(a, n);
-	return 0;
+    }
+    while(n <= 0 || n >= 100);
+    int a[n];
+    NhapMang(a, n);
+    XoaTatCaCacSoDenDui(a, n);
+    SapXep(a, n);
+    XuatMang(a, n);
+    return 0;
 }
--- K16-C - C++/BTVN5/Bai2_BTVN6.cpp
+++ K16-C - C++/BTVN5/Bai2_BTVN6.cpp
@@ -4,34 +4,38 @@ void NhapN(int &n)   /// truyen &n de sau khi nhap thi ra khoi ham n van giu ddc
 {
     do
     {
-		cout << "n = ";cin >> n;
-		if(n<=0 || n>=100)
-			cout << "Nhap sai, Nhap lai ! ";
-	}while(n<=0 || n>=100);
+        cout << "n = ";
+        cin >> n;
+        if(n<=0 || n>=100)
+            cout << "Nhap sai, Nhap lai ! ";
+    }
+    while(n<=0 || n>=100);
 
 }
-void NhapMang(int a[], int n){
-	for(int i = 0; i < n; i++)
+void NhapMang(int a[], int n)
+{
+    for(int i = 0; i < n; i++)
         cin >> a[i];
 }
-void XuatMang(int a[], int n){
+void XuatMang(int a[], int n)
+{
     for (int i = 0; i < n; i++)
-       cout << a[i] << " ";
+        cout << a[i] << " ";
 }
 
 void ChenChiSo(int a[], int &n, int viTri, int giaTri)/// Chen lam tang so luong phan tu nen can &n
 {
-	for(int i=n ; i>viTri ;i--)   /// dan mang ra 1 phan tu ve phia cuoi
+    for(int i=n ; i>viTri ; i--)  /// dan mang ra 1 phan tu ve phia cuoi
         a[i] = a[i-1];
     n++;
     a[viTri] = giaTri;
 }
 
-void PhanA(int a[], int &n , int x)  /// Chua ham chen ... nen can &n
+void PhanA(int a[], int &n, int x)   /// Chua ham chen ... nen can &n
 {
     int i=0;
     while(a[i]>0 && i<n-1) /// Tim so am dau tien thi dung lai
-         i++;
+        i++;
     ChenChiSo(a,n,i+1,x); /// neu ma i < n chung to co xuat hien so am trong mang , khong thi nguoc lai ( +1 ) la chen vao sau
 
 }
@@ -57,19 +61,20 @@ void HoanViCPP(int &a, int &b)  // void ngon ngu C++ , goi ham nhu sau HoanViCPP
 /// Cach 5 : Su dung toan tu voi bit a^=b^=a^=b; // KHONG nen dung cach nay
 
 /// Cach 6 : a = a+b;b = a-b;a = a-b; // Su dung toan hoc
-void SapXep(int a[], int n) {
-	for(int i=0; i<n; i++)
+void SapXep(int a[], int n)
+{
+    for(int i=0; i<n; i++)
     {
-		for(int j=i+1; j<n; j++)
+        for(int j=i+1; j<n; j++)
         {
-			if(a[i] < a[j])
-			{
-				int temp = a[i];   /// Cach 7 dung bien trung gian
-				a[i] = a[j];
-				a[j] = temp;
-			}
-		}
-	}
+            if(a[i] < a[j])
+            {
+                int temp = a[i];   /// Cach 7 dung bien trung gian
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
 }
 
 void PhanB(int a[], int n)
@@ -84,30 +89,31 @@ void PhanB(int a[], int n)
         i++;
     }
     while(viTriSoLonNhatCuoiCung < viTriSoBeNhatDauTien)  /// duyet tu phan tu lon nhat cuoi cung den phan tu be nhat dau tien , vd mang 8,8,4,3,2,1 -> duuyet tu 4->6 va hoan vi dau va cuoi ta duoc 8,8, 2,3,4,1
-        HoanViCPP( a[viTriSoLonNhatCuoiCung++] ,a[viTriSoBeNhatDauTien--] );
+        HoanViCPP( a[viTriSoLonNhatCuoiCung++],a[viTriSoBeNhatDauTien--] );
 
 }
-int main(){
+int main()
+{
 
-	int n, x;
-	NhapN(n);
-	int a[n+1];  /// Khai bao thua 1 phan tu vi bai nay co chen them 1 phan tu
-	NhapMang(a, n);
-	cout << "\nMang da nhap la: ";
-	XuatMang(a, n);
+    int n, x;
+    NhapN(n);
+    int a[n+1];  /// Khai bao thua 1 phan tu vi bai nay co chen them 1 phan tu
+    NhapMang(a, n);
+    cout << "\nMang da nhap la: ";
+    XuatMang(a, n);
 
-	///Phan A
-	cout << "\n\nSo muon chen: ";
-	cin >> x;
+    ///Phan A
+    cout << "\n\nSo muon chen: ";
+    cin >> x;
     PhanA(a, n, x);
-	cout << "\nPhan a: Mang sau khi them x: ";
-	XuatMang(a, n);
+    cout << "\nPhan a: Mang sau khi them x: ";
+    XuatMang(a, n);
 
-	///Phan B
+    ///Phan B
     SapXep(a,n);
     PhanB(a,n);
     cout << "\nPhan b: Mang sau khi sap xep : ";
     XuatMang(a, n);
 
-	return 0;
+    return 0;
 }
